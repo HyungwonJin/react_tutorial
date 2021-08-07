3d7d0f19837dd1d6c5dacf50ebbf8507f6e12646
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-08-07 15:29:22 +0900

    NumberBaseball_Class

--- client.jsx
+++ client.jsx
@@ -1,7 +1,5 @@
-const React = require("react");
-const ReactDom = require("react-dom");
-const WordRelay = require("./WordRelay");
+import React from "react";
+import ReactDOM from "react-dom";
+import NumberBaseball from "./NumberBaseball";
 
-
-
-ReactDom.render(<WordRelay />, document.querySelector("#root"));
+ReactDOM.render(<NumberBaseball />, document.querySelector("#root"));
