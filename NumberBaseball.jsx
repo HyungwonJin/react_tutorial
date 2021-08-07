0932ae75307902d934bf736f553c3da209776360
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-08-07 21:38:10 +0900

    class -> Hook

--- NumberBaseball.jsx
+++ NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useRef, useState } from "react";
 import Try from "./Try";
 
 function getNumbers() {
@@ -11,82 +11,95 @@ function getNumbers() {
     return array;
 }
 
-class NumberBaseball extends Component {
-    state = {
-        result: '',
-        value: '',
-        answer: getNumbers(),
-        tries: [],
-    }
+const NumberBaseball = () => {
+    const [result, setResult] = useState('');
+    const [value, setValue] = useState('');
+    const [answer, setAnswer] = useState(getNumbers());
+    const [tries, setTries] = useState([]);
+    const inputEl = useRef(null);
 
-    onSubmitForm = (e) => {
+    const onSubmitForm = (e) => {
         e.preventDefault();
-        if (this.state.value === this.state.answer.join('')) {
-            this.setState({
-                result: "홈런!",
-                tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }],
+        if (value === answer.join('')) {
+            setResult("홈런!");
+            setTries((prevTries) => {
+                return [...prevTries, { try: value, result: "홈런!" }]
             })
+            // this.setState((prevState) => {
+            //     return {
+            //         result: "홈런!",
+            //         tries: [...prevState.tries, { try: value, result: '홈런!' }],
+            //     }
+            // })
             alert('게임을 다시 시작합니다.');
-            this.setState({
-                value: '',
-                answer: getNumbers(),
-                tries: [],
-            });
+            setValue("");
+            setAnswer(getNumbers());
+            setTries([]);
+            inputEl.current.focus();
+            // this.setState({
+            //     value: '',
+            //     answer: getNumbers(),
+            //     tries: [],
+            // });
         } else { // 답 틀렸으면
-            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            const answerArray = value.split('').map((v) => parseInt(v));
             let strike = 0;
             let ball = 0;
-            if (this.state.tries.length >= 9) {
-                this.setState({
-                    result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(",")}였습니다!`,
-                })
+            if (tries.length >= 9) {
+                setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(",")}였습니다!`)
+                // this.setState({
+                //     result: `10번 넘게 틀려서 실패! 답은 ${answer.join(",")}였습니다!`,
+                // })
                 alert('게임을 다시 시작합니다.');
-                this.setState({
-                    value: '',
-                    answer: getNumbers(),
-                    tries: [],
-                });
+                setValue('');
+                setAnswer(getNumbers());
+                setTries([]);
+                inputEl.current.focus();
+                // this.setState({
+                //     value: '',
+                //     answer: getNumbers(),
+                //     tries: [],
+                // });
             } else {
                 for (let i = 0; i < 4; i += 1) {
-                    if (answerArray[i] === this.state.answer[i]) {
+                    if (answerArray[i] === answer[i]) {
                         strike += 1;
-                    } else if (this.state.answer.includes(answerArray[i])) {
+                    } else if (answer.includes(answerArray[i])) {
                         ball += 1;
                     }
                 }
-                this.setState({
-                    tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼 입니다.` }],
-                    value: '',
-                })
+                setTries((prevTries) => [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼 입니다.` }])
+                setValue("");
+                // this.setState((prevState) => {
+                //     return {
+                //         tries: [...prevState.tries, { try: value, result: `${strike} 스트라이크, ${ball} 볼 입니다.` }],
+                //         value: '',
+                //     }
+                // })
             }
         }
     };
 
-    onChangeInput = (e) => {
-        this.setState({
-            value: e.target.value,
-        })
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
     };
 
-
-    render() {
-        return (
-            <>
-                <h1>{this.state.result}</h1>
-                <form onSubmit={this.onSubmitForm}>
-                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
-                </form>
-                <div>시도: {this.state.tries.length}</div>
-                <ul>
-                    {this.state.tries.map((v, i) => {
-                        return (
-                            <Try key={`${i + 1}차 시도`} tryInfo={v} />
-                        );
-                    })}
-                </ul>
-            </>
-        )
-    }
+    return (
+        <>
+            <h1>{result}</h1>
+            <form onSubmit={onSubmitForm}>
+                <input ref={inputEl} maxLength={4} value={value} onChange={onChangeInput} />
+            </form>
+            <div>시도: {tries.length}</div>
+            <ul>
+                {tries.map((v, i) => {
+                    return (
+                        <Try key={`${i + 1}차 시도`} tryInfo={v} />
+                    );
+                })}
+            </ul>
+        </>
+    )
 }
 
 export default NumberBaseball;
--- Try.jsx
+++ Try.jsx
@@ -1,14 +1,24 @@
-import React, { Component } from "react";
+import React, { memo } from "react";
 
-class Try extends Component {
-    render() {
-        return (
-            <li>
-                <div>{this.props.tryInfo.try}</div>
-                <div>{this.props.tryInfo.result}</div>
-            </li>
-        )
-    }
+const Try = memo(({ tryInfo }) => {
+    return (
+        <li>
+            <div>{tryInfo.try}</div>
+            <div>{tryInfo.result}</div>
+        </li>
+    )
 }
+)
+
+// class Try extends Component {
+//     render() {
+//         return (
+//             <li>
+//                 <div>{this.props.tryInfo.try}</div>
+//                 <div>{this.props.tryInfo.result}</div>
+//             </li>
+//         )
+//     }
+// }
 
 export default Try;
