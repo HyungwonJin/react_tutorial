8ab033a481e3ec36bcc7b7d71bddced091c64b12
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-08-09 16:27:04 +0900

    반응속도 class -> Hooks

--- ResponseCheck.jsx
+++ ResponseCheck.jsx
@@ -1,75 +1,65 @@
-import React, { Component } from "react";
+import React, { useRef, useState } from "react";
 
-class ResponseCheck extends Component {
-    state = {
-        state: 'waiting',
-        message: '클릭해서 시작하세요.',
-        result: [],
-    }
+const ResponseCheck = () => {
+    const [state, setState] = useState('waiting');
+    const [message, setMessage] = useState("클릭해서 시작하세요,");
+    const [result, setResult] = useState([]);
+    const timeout = useRef(null);
+    const startTime = useRef(0);
+    const endTime = useRef(0);
 
-    timeout;
-    startTime;
-    endTime;
-
-    onClickScreen = () => {
-        const { state, message, result } = this.state;
+    const onClickScreen = () => {
         if (state === 'waiting') {
-            this.setState({
-                state: 'ready',
-                message: '초록색이 되면 클릭하세요.',
-            });
-            this.timeout = setTimeout(() => {
-                this.setState({
-                    state: 'now',
-                    message: '지금 클릭',
-                })
-                this.startTime = new Date();
+            setState('ready');
+            setMessage("초록색이 되면 클릭하세요.")
+            timeout.current = setTimeout(() => {
+                // this.timeout = setTimeout(() => {
+                setState('now');
+                setMessage('지금 클릭');
+                startTime.current = new Date();
+                // this.startTime = new Date();
             }, Math.floor(Math.random() * 1000) + 2000);
         } else if (state === 'ready') {
-            clearTimeout(this.timeout);
-            this.setState({
-                state: 'waiting',
-                message: '너무 일찍 눌렀습니다.'
-            })
+            clearTimeout(timeout.current);
+            // clearTimeout(this.timeout);
+            setState('waiting');
+            setMessage('너무 일찍 눌렀습니다.');
         } else if (state === 'now') {
-            this.endTime = new Date();
-            this.setState((prevState) => {
-                return {
-                    state: 'waiting',
-                    message: '클릭해서 시작하세요.',
-                    result: [...prevState.result, this.endTime - this.startTime],
-                }
+            endTime.current = new Date();
+            // this.endTime = new Date();
+            setState('waiting');
+            setMessage('클릭해서 시작하세요.')
+            setResult((prevState) => {
+                return [...prevState, endTime.current - startTime.current];
+                // result: [...prevState.result, this.endTime - this.startTime];
             })
         }
     }
-
-    onReset = () => [
-        this.setState({
-            result: [],
-        })
-    ]
-
-
-    render() {
-        const { state, message, result } = this.state;
-        return (
-            <>
-                <div
-                    id="screen"
-                    className={state}
-                    onClick={this.onClickScreen}
-                >
-                    {message}
-                </div>
-                {result.length === 0 ? null :
-                    <>
-                        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
-                        <button onClick={this.onReset}>리셋</button>
-                    </>
-                }
-            </>
-        )
+    const onReset = () => {
+        setResult([]);
     }
+
+    const renderAverage = () => {
+        return result.length === 0
+            ? null
+            : <>
+                <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+                <button onClick={onReset}>리셋</button>
+            </>
+    };
+
+    return (
+        <>
+            <div
+                id="screen"
+                className={state}
+                onClick={onClickScreen}
+            >
+                {message}
+            </div>
+            {renderAverage()}
+        </>
+    )
 
 }
 
