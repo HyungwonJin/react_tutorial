588716ed22b42ea935f715c418bdbad2c6270507
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-08-10 16:16:11 +0900

    Update RSP.jsx

--- RSP.jsx
+++ RSP.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const rspCoords = {
     바위: '0',
@@ -24,6 +24,13 @@ const RSP = () => {
     const [score, setScore] = useState(0);
     const interval = useRef();
 
+    useEffect(() => {
+        interval.current = setInterval(changeHand, 100);
+        return () => {
+            clearInterval(interval.current);
+        }
+    }, [imgCoord]);
+
     const changeHand = () => {
         if (imgCoord === rspCoords.바위) {
             setImgCoord(rspCoords.가위)
@@ -34,24 +41,27 @@ const RSP = () => {
         }
     }
 
-    const onClickBtn = (choice) => {
-        clearInterval(interval.current);
-        const myScore = scores[choice];
-        const cpuScore = scores[computerChoice(imgCoord)];
-        const diff = myScore - cpuScore;
-        if (diff === 0) {
-            setResult('비겼습니다.')
-        } else if ([-1, 2].includes(diff)) {
-            setResult('이겼습니다.');
-            setScore((prevScore) => prevScore + 1)
-        } else {
-            setResult("졌습니다.");
-            setScore((prevScore) => prevScore - 1)
+    const onClickBtn = (choice) => () => {
+        if (interval.current) {
+            clearInterval(interval.current);
+            interval.current = null;
+            const myScore = scores[choice];
+            const cpuScore = scores[computerChoice(imgCoord)];
+            const diff = myScore - cpuScore;
+            if (diff === 0) {
+                setResult('비겼습니다!');
+            } else if ([-1, 2].includes(diff)) {
+                setResult('이겼습니다!');
+                setScore((prevScore) => prevScore + 1);
+            } else {
+                setResult('졌습니다!');
+                setScore((prevScore) => prevScore - 1);
+            }
+            setTimeout(() => {
+                interval.current = setInterval(changeHand, 100);
+            }, 1000);
         }
-        setTimeout(() => {
-            interval.current = setInterval(changeHand, 100);
-        }, 1000);
-    }
+    };
 
     return (
         <>
@@ -65,88 +75,6 @@ const RSP = () => {
             <div>현재 {score}점</div>
         </>
     );
-}
-class RSP extends Component {
-    state = {
-        result: '',
-        imgCoord: rspCoords.바위,
-        score: 0,
-    }
-
-    interval;
-
-    componentDidMount() { // 컴포넌트가 첫 렌더링 된 후, 여기에 비동기 요청을 많이 함
-        this.interval = setInterval(this.changeHand, 100)
-    }
-
-    componentWillUnmount() { // 컴포넌트가 제거되기 직전, 비동기 요청 정리르 많이 함
-        clearInterval(this.interval);
-    }
-
-    changeHand = () => {
-        console.log('hello', this.state.imgCoord, rspCoords.가위);
-        const { imgCoord } = this.state;
-        if (imgCoord === rspCoords.바위) {
-            this.setState({
-                imgCoord: rspCoords.가위,
-            });
-        } else if (imgCoord === rspCoords.가위) {
-            console.log('here');
-            this.setState({
-                imgCoord: rspCoords.보,
-            });
-        } else if (imgCoord === rspCoords.보) {
-            this.setState({
-                imgCoord: rspCoords.바위,
-            });
-        }
-    }
-
-    onClickBtn = (choice) => () => {
-        const { imgCoord } = this.state;
-        clearInterval(this.interval);
-        const myScore = scores[choice];
-        const cpuScore = scores[computerChoice(imgCoord)];
-        const diff = myScore - cpuScore;
-        if (diff === 0) {
-            this.setState({
-                result: '비겼습니다.',
-            })
-        } else if ([-1, 2].includes(diff)) {
-            this.setState((prevState) => {
-                return {
-                    result: "이겼습니다.",
-                    score: prevState.score + 1,
-                };
-            });
-        } else {
-            this.setState((prevState) => {
-                return {
-                    result: "졌습니다.",
-                    score: prevState.score - 1,
-                };
-            });
-        }
-        setTimeout(() => {
-            this.interval = setInterval(this.changeHand, 100);
-        }, 1000);
-    };
-
-    render() {
-        const { result, score, imgCoord } = this.state;
-        return (
-            <>
-                <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
-                <div>
-                    <button id="rock" className='btn' onClick={this.onClickBtn('바위')}>바위</button>
-                    <button id="scissor" className='btn' onClick={this.onClickBtn('가위')}>가위</button>
-                    <button id="paper" className='btn' onClick={this.onClickBtn('보')}>보</button>
-                </div>
-                <div>{result}</div>
-                <div>현재 {score}점</div>
-            </>
-        )
-    }
 }
 
 export default RSP;
